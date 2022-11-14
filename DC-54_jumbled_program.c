be5f6c71a627c20f38ba3ed032e2eb001a6ab41f
keerthivasan <93980178+rmkv007@users.noreply.github.com>
2022-11-15 02:13:56 +0530

    Add files via upload

---
+++ DC-54_jumbled_program.c
@@ -0,0 +1,58 @@
+/*
+	The program given below perform certain logic
+	but the lines are jumbled. Reorder the lines
+	so that the program runs ssuccessfully.
+	
+	#include<stdio.h>
+	{
+	   if(product%sum==0)
+	   {
+	   		int main()
+       }
+       else
+       {
+       		product*N%10;
+       		printf("No");
+       }
+    }
+    int N,sum=0,product=1;
+    while(N>0)
+    {
+    	printf("Yes");
+    	sum+=N%10;
+    }
+    if(N%2==0)
+    {
+    	N/=10;
+    }
+    else
+    {
+    	scanf("%d",&N);
+    }
+*/
+#include<stdio.h>
+int main()
+{
+	int N,sum=0,product=1;
+	scanf("%d",&N);
+	while(N>0)
+	{
+		if(N%2==0)
+		{
+			product*=N%10;
+		}
+		else
+		{
+			sum+=N%10;
+		}
+		N/=10;
+    }
+	if(product%sum==0)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");
+	}
+}
